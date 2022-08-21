323a52c820bf6e7e7bd67bd5f7e24e053212a444
zapata124 <ecz001@live.com>
2022-08-21 14:19:11 -0400

    Adding bulletchart

---
+++ src/Charts/BulletChart.jsx
@@ -0,0 +1,21 @@
+// install (please make sure versions match peerDependencies)
+// yarn add @nivo/core @nivo/bullet
+import { ResponsiveBullet } from '@nivo/bullet'
+
+// make sure parent container have a defined height when using
+// responsive component, otherwise height will be 0 and
+// no chart will be rendered.
+// website examples showcase many properties,
+// you'll often use just a few of them.
+const BulletChart = ({ data /* see data tab */ }) => (
+    <ResponsiveBullet
+        data={data}
+        margin={{ top: 50, right: 90, bottom: 50, left: 90 }}
+        spacing={46}
+        titleAlign="start"
+        titleOffsetX={-70}
+        measureSize={0.2}
+    />
+)
+
+export default BulletChart;
