59b53f5c36caa4dd35640ff13bf76d54e9d6bce7
zapata124 <ecz001@live.com>
2022-08-31 20:14:53 -0400

    Making hover effect for cards

--- src/reusables/ChartContainer/ChartContainer.jsx
+++ src/reusables/ChartContainer/ChartContainer.jsx
@@ -1,15 +1,39 @@
 import React from "react";
-import { Box, Grid, Card } from "@mui/material";
+import {
+  Box,
+  Grid,
+  Card,
+  CardContent,
+  CardActionArea,
+  Paper,
+} from "@mui/material";
+import { hover } from "@testing-library/user-event/dist/hover";
 
+const CardHover = ({ children }) => {
+  const [hovered, setHovered] = React.useState(false);
+  return (
+    <Card
+      elevation={hovered ? 8 : 2}
+      onMouseOver={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
+      {children}
+    </Card>
+  );
+};
 const ChartContainer = ({ viz }) => {
   return (
     <>
       {viz.map((element) => {
         return (
           <Grid item>
-            <Card>
-              <Box sx={{ width: "300px", height: "300px" }}>{element}</Box>
-            </Card>
+            <CardHover>
+              {/* <CardActionArea> */}
+              <CardContent>
+                <Box sx={{ width: "300px", height: "300px" }}>{element}</Box>
+              </CardContent>
+              {/* </CardActionArea> */}
+            </CardHover>
           </Grid>
         );
       })}
