c196c34ed9a4bb98fe5290401e6863e08e0e0a96
zapata124 <ecz001@live.com>
2022-09-07 19:20:40 -0400

    framer moition practice

--- src/reusables/ChartContainer/ChartContainer.jsx
+++ src/reusables/ChartContainer/ChartContainer.jsx
@@ -12,7 +12,7 @@ import {
   DialogContent,
 } from "@mui/material";
 import { hover } from "@testing-library/user-event/dist/hover";
-
+import { motion } from "framer-motion";
 const PaperHover = ({ children, title }) => {
   const [hovered, setHovered] = React.useState(false);
   const [dimensions, setDimensions] = React.useState("300px");
@@ -24,7 +24,7 @@ const PaperHover = ({ children, title }) => {
   return (
     <>
       <Paper
-        elevation={hovered ? 8 : 2}
+        // elevation={hovered ? 8 : 2}
         onMouseOver={() => setHovered(true)}
         onMouseLeave={() => setHovered(false)}
         onClick={() => setOpen(true)}
@@ -97,14 +97,31 @@ const ChartContainer = ({ viz }) => {
       {viz.map((element, index) => {
         return (
           <Grid item key={index} draggable="true" ondragstart={(e) => drag(e)}>
-            <PaperHover
-              title={element.type.name.substring(
-                0,
-                element.type.name.length - 5
-              )}
+            <motion.div
+              animate={{
+                scale: [1, 2, 2, 1, 1],
+                rotate: [0, 0, 270, 270, 0],
+                borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+              }}
+              // whileHover={{ scale: 1.1 }}
+              // whileTap={{ scale: 0.9 }}
+              // drag
+              // dragConstraints={{
+              //   top: -50,
+              //   left: -50,
+              //   right: 50,
+              //   bottom: 50,
+              // }}
             >
-              {element}
-            </PaperHover>
+              <PaperHover
+                title={element.type.name.substring(
+                  0,
+                  element.type.name.length - 5
+                )}
+              >
+                {element}
+              </PaperHover>
+            </motion.div>
           </Grid>
         );
       })}
